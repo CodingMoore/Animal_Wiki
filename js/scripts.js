98ff877d5061a00bc40b0e329a7a539f458bfb11
Randel M <Randel.C.Moore@gmail.com>
2020-10-22 11:55:21 -0700

    add frog images, text, and branched drop down menu
    Co-authored-by: Joe Nero <josephnero111@gmail.com>
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  
-})
+  $("#animalType").change(function() {
+    const animal = $("#animalType").val();
+    $("#parent div").hide();
+    if (animal === "Transparent Frog") {
+      $("#transparentFrog").show();
+    } else if (animal === "Crucifix Frog") {
+      $("#crucifixFrog").show();
+    } else {
+      $("#turtleFrog").show();
+    }
+  });
+});
